71d0318bf24c01a9431dec55e8ce9945dd3c7397
K.Ivanov <v.b.ingram@gmail.com>
2019-09-27 00:09:07 +0300

    remove dbrain logo

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Logo, CrossIcon, RossetiIcon } from 'components/Icons';
+import { RossetiIcon } from 'components/Icons';
 import './style.scss';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
@@ -9,14 +9,8 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
       <div className="header">
         <div className="logo">
           <Link to="/">
-            <Logo />
+            <RossetiIcon />
           </Link>
-        </div>
-        <div className="delimiter">
-          <CrossIcon />
-        </div>
-        <div className="moesk">
-          <RossetiIcon />
         </div>
       </div>
     );
